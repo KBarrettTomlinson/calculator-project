6c6c35712e90d20b364c8d20f3ee0303d5fbe7d9
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-18 14:21:18 -0500

    adds basic on-click event listeners to the operator buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,29 @@
+var operation = {};
+
 $(document).ready(function(){
   console.log("I'm Here For You.");
 
+  eventListeners();
 
 });//ends document ready
+
+function eventListeners(){
+  $('#adds').on("click",function(){
+    console.log("Alright, let do some adding!");
+  });//ends adds button listener
+
+  $('#subtracts').on("click",function(){
+    console.log("Alright, let do some subtracting!");
+  });//ends subtracts button listener
+
+  $('#multiplies').on("click",function(){
+    console.log("Alright, let do some multiplying!");
+  });//ends multiplies button listener
+
+  $('#divides').on("click",function(){
+    console.log("Alright, let do some dividing!");
+  });//ends divides button listener
+
+
+
+}//ends eventListeners function
